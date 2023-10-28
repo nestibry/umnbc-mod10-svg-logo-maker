0e1fd7d80f4d53e26f76e7c23658b2171a2c38dc
nestibry <nestibry@gmail.com>
2023-10-27 22:22:56 -0500

    updated square  with quotes around the colors

--- test/Square.test.js
+++ test/Square.test.js
@@ -31,7 +31,7 @@ describe("Square", () => {
             const newShape = new Square(mytext, mytextcolor, myshape, myshapecolor);
 
 
-            const expectedSvgStr = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="50" y="0" rx="0" ry="0" width="200" height="200" fill=${myshapecolor}/><text x="150" y="120" font-size="65" text-anchor="middle" fill=${mytextcolor}>${mytext}</text></svg>`;
+            const expectedSvgStr = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="50" y="0" rx="0" ry="0" width="200" height="200" fill="${myshapecolor}"/><text x="150" y="120" font-size="65" text-anchor="middle" fill="${mytextcolor}">${mytext}</text></svg>`;
     
             expect(newShape.render()).toEqual(expectedSvgStr);
         });
