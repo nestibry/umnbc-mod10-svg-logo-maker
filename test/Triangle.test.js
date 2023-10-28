40c8bd25a0511abc8c2bcc3cdbbfea45b740c9e3
nestibry <nestibry@gmail.com>
2023-10-27 22:24:10 -0500

    updated triangle  with quotes around the colors

--- test/Triangle.test.js
+++ test/Triangle.test.js
@@ -31,7 +31,7 @@ describe("Triangle", () => {
             const newShape = new Triangle(mytext, mytextcolor, myshape, myshapecolor);
 
 
-            const expectedSvgStr = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="0, 0 300, 0 150, 200" fill=${myshapecolor}/><text x="150" y="65" font-size="65" text-anchor="middle" fill=${mytextcolor}>${mytext}</text></svg>`;
+            const expectedSvgStr = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="0, 0 300, 0 150, 200" fill="${myshapecolor}"/><text x="150" y="65" font-size="65" text-anchor="middle" fill="${mytextcolor}">${mytext}</text></svg>`;
     
         
             expect(newShape.render()).toEqual(expectedSvgStr);
