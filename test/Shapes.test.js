e88e17ef66f3f77b1fabd35128298c9597d98c21
nestibry <nestibry@gmail.com>
2023-10-27 20:42:18 -0500

    Rectangle class and test complete

--- test/Shapes.test.js
+++ test/Shapes.test.js
@@ -1,7 +1,7 @@
 const Shape = require('../lib/Shapes.js');
 // const Circle = require('../lib/Shapes.js');
 const Triangle = require('../lib/Shapes.js');
-const Rectangle = require('../lib/Shapes.js');
+// const Rectangle = require('../lib/Shapes.js');
 
 
 describe("Shape", () => {
@@ -30,43 +30,6 @@ describe("Shape", () => {
 
 });
 
-// describe("Circle", () => {
-
-//     describe("instantiation", () => {
-//         it("should instantiate correctly", () => {
-//             const text = "BMW";
-//             const textcolor = "white";
-//             const shape = "circle";
-//             const shapecolor = "blue";
-//             const newShape = new Circle(text, textcolor, shape, shapecolor);
-//             expect(newShape).toBeInstanceOf(Circle);
-//         });
-//     });
-
-//     it("the number of elements must match exactly", () => {
-//         const text = "BMW";
-//         const textcolor = "white";
-//         const shape = "circle";
-//         const shapecolor = "blue";
-//         const newShape = new Circle(text, textcolor, shape, shapecolor);
-//         expect(newShape).toMatchObject({text:text, textcolor:textcolor, shape:shape, shapecolor:shapecolor});
-//     });
-
-//     // it("should return the svgStr correctly", async () => {
-//     //     const mytext = "BMW";
-//     //     const mytextcolor = "white";
-//     //     const myshape = "circle";
-//     //     const myshapecolor = "blue";
-//     //     const mysvgStr = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="100" fill=${myshapecolor} /><text x="150" y="120" font-size="60" text-anchor="middle" fill=${mytextcolor}>${mytext}</text></svg>`;
-
-//     //     const newCircle = new Circle(mytext, mytextcolor, myshape, myshapecolor);
-//     //     console.log(newCircle);
-
-//     //     expect(newCircle).toBe(myshape);
-//     // });
-
-
-// });
 
 describe("Triangle", () => {
 
@@ -93,28 +56,28 @@ describe("Triangle", () => {
 
 });
 
-describe("Rectangle", () => {
+// describe("Rectangle", () => {
 
-    describe("instantiation", () => {
+//     describe("instantiation", () => {
 
-        it("should instantiate correctly", () => {
-            const text = "BMW";
-            const textcolor = "white";
-            const shape = "rectangle";
-            const shapecolor = "blue";
-            const newShape = new Rectangle(text, textcolor, shape, shapecolor);
-            expect(newShape).toBeInstanceOf(Rectangle);
-        });
+//         it("should instantiate correctly", () => {
+//             const text = "BMW";
+//             const textcolor = "white";
+//             const shape = "rectangle";
+//             const shapecolor = "blue";
+//             const newShape = new Rectangle(text, textcolor, shape, shapecolor);
+//             expect(newShape).toBeInstanceOf(Rectangle);
+//         });
 
-        it("the number of elements must match exactly", () => {
-            const text = "BMW";
-            const textcolor = "white";
-            const shape = "rectangle";
-            const shapecolor = "blue";
-            const newShape = new Rectangle(text, textcolor, shape, shapecolor);
-            expect(newShape).toMatchObject({text:text, textcolor:textcolor, shape:shape, shapecolor:shapecolor});
-        });
-    });
+//         it("the number of elements must match exactly", () => {
+//             const text = "BMW";
+//             const textcolor = "white";
+//             const shape = "rectangle";
+//             const shapecolor = "blue";
+//             const newShape = new Rectangle(text, textcolor, shape, shapecolor);
+//             expect(newShape).toMatchObject({text:text, textcolor:textcolor, shape:shape, shapecolor:shapecolor});
+//         });
+//     });
 
 
-});
+// });
